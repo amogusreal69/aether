f006c26ce26737b6ee2efeb4d35d332e523f260b
amogus <amogusreal69420@proton.me>
2025-11-19 19:02:08 +0000

    fix velocity config

--- entrypoint.sh
+++ entrypoint.sh
@@ -404,6 +404,7 @@ function install_velocity {
     create_config "mc_proxy_velocity"
     echo -e "\e[38;2;129;170;254m[INFO] \e[38;5;250mDownloading default Velocity config...\e[0m"
     curl -o $HOME/velocity.toml https://aether.loners.software/files/velocity.toml
+    sed -i "s/serverport/$SERVER_PORT/g" "$HOME/velocity.toml"
     jar_bytes=$(stat -c%s server.jar 2>/dev/null || stat -f%z server.jar 2>/dev/null)
     jar_size=$(printf "%.2f MB" $((jar_bytes / 1000000)))
     echo -e "\e[38;2;129;170;254m[INFO] \e[38;5;250mServer jar downloaded successfully (Size: $jar_size)\e[0m"
