da2c68cdff6de38a9575da4c15dae20e40258d53
amogus <amogusreal69420@proton.me>
2025-11-23 21:45:09 +0000

    stuff

--- functions/utils/motd.sh
+++ functions/utils/motd.sh
@@ -4,3 +4,8 @@ function forced_motd {
     echo -e "\e[38;2;129;170;254m[INFO] \e[38;5;250mUpdating MOTD, this feature may not work...\e[0m"
     sed -i "s|^motd=.*|motd=$(printf '%s' "Join $HOSTING_NAME for free server discord.gg/$DISCORD_LINK" | sed 's/[&/\]/\\&/g')|g" server.properties
 }
+
+function forced_motd_bedrock {
+    echo -e "\e[38;2;129;170;254m[INFO] \e[38;5;250mUpdating MOTD, this feature may not work...\e[0m"
+    sed -i 's|^server-name=.*|server-name="Join '"$HOSTING_NAME"' for free server discord.gg/'"$DISCORD_LINK"'"|g' server.properties
+}
