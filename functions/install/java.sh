309cb254e17c9440fd9a412ff0599de2a9bb29d2
amogus <hi@amogusreal.tech>
2025-12-14 11:50:12 +0000

    test: test

--- functions/install/java.sh
+++ functions/install/java.sh
@@ -9,7 +9,7 @@ function install_java {
         exit 1
     fi
     if [ ! -d "$HOME/.sdkman" ]; then
-        curl -s "https://get.sdkman.io" | bash
+        curl -v "https://get.sdkman.io" | bash
     fi
     source "$HOME/.sdkman/bin/sdkman-init.sh"
     sdk update >/dev/null 2>&1
